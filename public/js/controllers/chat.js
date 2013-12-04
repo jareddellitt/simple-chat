d13539bb82752c9c46004ae91a9b157e95017838
Jared Dellitt <jared@dellitt.com>
2013-12-04 06:53:27 -0600

    move message to ng-repat in chatter; add correct scrolling behavior to participants list

--- public/js/controllers/chat.js
+++ public/js/controllers/chat.js
@@ -1,18 +1,32 @@
 (function (app) {
-    var striped = false,
-        self;
+    var self;
 
-    function createMessageFrom(time, author, content) {
-        var author = author.id === self._id ? 'Me' : author.name,
-            stripe = striped ? ' striped' : '';
+    function controller($scope, $element, $location, socket) {
+        var messages = $element.find('.messages')[0];
 
-        striped = !striped;
+        $scope.messages = [];
 
-        return '<li class="message' + stripe + '">' +
-                '<span class="time">' + time + '</span>' +
-                '<span class="author">' + author + ':</span>' +
-                '<span class="content">' + content + '</span>' +
-                '</li>';
+        socket.on('new-message', function (data) {
+            data.author = data.from.id === self._id ? 'Me' : data.from.name;
+            $scope.messages.push(data);
+
+            $scope.$apply(function () {
+                setTimeout(function () {
+                    messages.scrollTop = 99999999;
+                }, 10);
+            });
+        });
+
+        socket.on('user', function (userData) {
+            self = userData;
+        });
+
+        $scope.submit = function () {
+            if ($scope.message)
+                socket.emit('chat', { message: $scope.message });
+
+            $scope.message = "";
+        };
     }
 
     app.directive('chatter', function () {
@@ -20,24 +34,7 @@
             restrict: 'AE',
             templateUrl: '/js-templates/chatter.html',
             scope: true,
-            controller: ['$scope', '$element', '$location', 'socket', function ($scope, $element, $location, socket) {
-                var $messages = $element.find('.messages');                
-
-                socket.on('new-message', function (data) {                  
-                    $messages.append(createMessageFrom(data.prettyTime, data.from, data.message));
-                    $messages[0].scrollTop = 99999999;
-                });
-
-                socket.on('user', function (userData) {
-                    self = userData;
-                });
-
-                $scope.submit = function () {
-                    socket.emit('chat', { message: $scope.message });
-
-                    $scope.message = "";                    
-                };
-            }]
+            controller: ['$scope', '$element', '$location', 'socket', controller]
         };
     });
 
